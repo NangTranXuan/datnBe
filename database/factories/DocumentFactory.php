06539fcca63e0bd510872e301ec4cf03c8f33383
nangtranxuan <nang.xuantran2001@gmail.com>
2024-04-28 14:27:55 +0700

    fix db

---
+++ database/factories/DocumentFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Lesson;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\File;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class DocumentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $directory = public_path('uploads');
+        $files = File::allFiles($directory);
+
+        $paths = [];
+        foreach ($files as $file) {
+            $paths[] = $file->getPathname();
+        }
+
+        return [
+            'link_url' => $this->faker->randomElement($paths),
+        ];
+    }
+}
---
+++ database/seeders/DocumentLessonSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\DocumentLesson;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DocumentLessonSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DocumentLesson::factory()->count(20)->create();
+
+    }
+}
